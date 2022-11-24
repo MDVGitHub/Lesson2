8a44d723168363054dfce353f6feaa151fd9106b
Dmitriy Makarov <d.makarov.mail@gmail.com>
2022-11-24 14:37:15 +0300

    dz2_1

---
+++ dz2_1/Program.cs
@@ -0,0 +1,8 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+Console.WriteLine("Введите число");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+string Number2 = Convert.ToString(Number1);
+Console.WriteLine($"Второй символ введенного числа это:" +Number2[1]);
